45bbad7cbb492b81bd0d2070edd6d72529ae8bd7
Michał Fijałkowski <michalfijalkowski@icloud.com>
2019-11-02 20:41:04 +0100

    Updated UI - menu and adding button

--- cities.php
+++ cities.php
@@ -3,11 +3,11 @@
   $postalCode = $_POST['postalCode'];
 
   // Database connection
-  $conn = new mysqli('localhost', 'root', 'password', 'marvex_system');
+  $conn = new mysqli('localhost', 'root', 'password', 'marvex');
   if ($conn->connect_error) {
     die('Połączenie z bazą danych nie powiodło się : '.$conn->connect_error);
   } else {
-    $stmt = $conn->prepare("insert into Miejscowosci(miejscowosc, kod_pocztowy) values(?, ?)");
+    $stmt = $conn->prepare("insert into miejscowosci(miejscowosc, kod_pocztowy) values(?, ?)");
     $stmt->bind_param("ss", $city, $postalCode);
     $stmt->execute();
     echo "Zapisanie danych do bazy powiodło się";
